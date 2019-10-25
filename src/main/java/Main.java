7fc2748a782df9981271a4b99e381224c89d1241
Phillip Xu <xiaoduoxu@outlook.com>
2019-10-25 15:21:45 +1100

    add google credentials

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 	public static int port = 9000;
 	public static void main(String[] args) {
 //		// start http server
-		SimpleHttpServer httpServer = new SimpleHttpServer();
-		httpServer.Start(port);
 		
+		
+
+		if(System.getenv("CREDENTIALS") != null) {
+			writeUsingFiles(System.getenv("CREDENTIALS"));
+			SimpleHttpServer httpServer = new SimpleHttpServer();
+			httpServer.Start(port);
+		} else {
+			System.out.println("credential not found.");
+		}
 		// start https server
 		// SimpleHttpsServer httpsServer = new SimpleHttpsServer();
 		// httpsServer.Start(port);
@@ -14,4 +29,12 @@ public class Main {
 //		System.out.println(Main.class.getClassLoader().getResource("").getPath());
 		
 	}
+
+	private static void writeUsingFiles(String data) {
+        try {
+            Files.write(Paths.get("/tmp/credentials.json"), data.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
